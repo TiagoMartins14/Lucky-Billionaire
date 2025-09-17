f08a81101e3e0150247e148263246935770e5916
Tiago Martins <tiago86martins@gmail.com>
2025-09-17 15:28:51 +0100

    updated css

--- Lucky-Bilionaire/src/App.tsx
+++ Lucky-Bilionaire/src/App.tsx
@@ -1,8 +1,10 @@
-// import React from 'react';
 import { sepolia } from 'wagmi/chains';
 import { useReadContract, useWriteContract } from 'wagmi';
 import { abi } from "./abi.ts";
-import './index.css';
+import React, { useState } from "react";
+import { Toggle } from "./components/Togle.css"
+
+import './App.css';
 
 const CONTRACT_ADDRESS = import.meta.env.CONTRACT_ADDRESS as `0x${string}`;
 const MIN_LUCKY_NUMBER = 1;
@@ -12,7 +14,7 @@ const MAX_LUCKY_NUMBER = 50;
 const InfoCard = ({ className, title, message }) => (
   <div className={`info-card ${className}`}>
     <h3>{title}</h3>
-    <div className="mt-2 text-gray-600">{message}</div>
+    <div>{message}</div>
   </div>
 );
 
@@ -83,6 +85,7 @@ function LuckyBilionaire() {
     return <div className="status-message error">Error fetching data. Check your network connection or contract address.</div>;
   }
 
+  const [isDark, setIsDark] = useState(false);
   // const [betNumber, setBetNumber] = useState('');
 
   // function Bet({ betNumber }: { betNumber: number }) {
@@ -112,85 +115,87 @@ function LuckyBilionaire() {
   const secondPlaceWinners = winnersBefore?.concat(winnersAfter ?? []) ?? [];
 
   return (
-    <div>
-      <div className="lucky-bilionaire-container">
-        <h1>
-          LUCKY BILIONAIRE
-        </h1>
-        <div className="container-flex">
-          {/* This container sets up the two main columns */}
-          <div className="main-flex-column">
-            <h2>LOTTERY RESULTS</h2>
-            <div className="info-section">
-              <InfoCard 
-                className="info-card" 
-                title="Lucky Number" 
-                message={lastWeekLuckyNumber?.toString() ?? "N/A"} 
-              />
-
-              <InfoCard
-                className="info-card" 
-                title="First Prize" 
-                message={`${lastWeekFirstPrize?.toString() ?? "N/A"} ETH`}
-              />
-
-              <InfoCard 
-                className="info-card" 
-                title="First Prize Winners" 
-                message={renderWinners(winnersExact)}
-              />
-
-              <InfoCard 
-                className="info-card" 
-                title="Second Prize" 
-                message={`${lastWeekSecondPrize?.toString() ?? "N/A"} ETH`}
-              />
-                        
-              <InfoCard 
-                className="info-card" 
-                title="Second Prize Winners" 
-                message={renderWinners(secondPlaceWinners)}
-              />
-            </div>
-          </div>
-            {/* This is the second column for the new features */}
+      <div className="App" data-theme={isDark ? "dark-mode" : "light-mode"}>
+        <button id="theme-switch">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>
+        </button>
+        <div className="lucky-bilionaire-container">
+          <h1>
+            LUCKY BILIONAIRE
+          </h1>
+          <div className="container-flex">
             <div className="main-flex-column">
-                <h2>BE THE NEXT BILIONAIRE</h2>
+              <h2>LOTTERY RESULTS</h2>
               <div className="info-section">
-                <div className="info-card">
-                  <h3>CURRENT JACKPOT</h3>
-                  <p className="highlighted-text">14 ETH</p>
-                </div>
-              </div>
-              <div className="action-section">
-                <div className="action-card">
-                  <h3>PLACE YOUR BET!</h3>
-                  <input placeholder="1 - 50"></input>
-                  <button className="bet-button">BET</button>
-                </div>
-                <div className="action-card">
-                  <h3>ARE YOU A LUCKY WINNER?</h3>
-                  <button>WITHDRAW YOUR PRIZE!</button>
-                </div>
-              </div>
-              {/* <div className="place-bet interaction-card">
-                <h3>PLACE YOUR BET</h3>
-                <input 
-                  type="number" 
-                  placeholder="1 - 50" 
-                  value={betNumber}
-                  onChange={(e) => setBetNumber(Number(e.target.value))}
+                <InfoCard 
+                  className="info-card" 
+                  title="Lucky Number" 
+                  message={lastWeekLuckyNumber?.toString() ?? "N/A"} 
                 />
-                <Bet betNumber={Number(betNumber)} />
+
+                <InfoCard
+                  className="info-card" 
+                  title="First Prize" 
+                  message={`${lastWeekFirstPrize?.toString() ?? "N/A"} ETH`}
+                />
+
+                <InfoCard 
+                  className="info-card" 
+                  title="First Prize Winners" 
+                  message={renderWinners(winnersExact)}
+                />
+
+                <InfoCard 
+                  className="info-card" 
+                  title="Second Prize" 
+                  message={`${lastWeekSecondPrize?.toString() ?? "N/A"} ETH`}
+                />
+                          
+                <InfoCard 
+                  className="info-card" 
+                  title="Second Prize Winners" 
+                  message={renderWinners(secondPlaceWinners)}
+                />
               </div>
-              <div className="withdraw-prize interaction-card">
-                <h3>ARE YOU A LUCKY WINNER?</h3>
-                <button className="withdraw-prize">Withdraw Prize!</button>
-              </div> */}
             </div>
+              <div className="main-flex-column">
+                  <h2>BE THE NEXT BILIONAIRE</h2>
+                <div className="info-section">
+                  <div className="info-card">
+                    <h3>CURRENT JACKPOT</h3>
+                    <p className="highlighted-text">14 ETH</p>
+                  </div>
+                </div>
+                <div className="action-section">
+                  <div className="action-card">
+                    <h3>PLACE YOUR BET!</h3>
+                    <input placeholder="1 - 50"></input>
+                    <button className="bet-button">BET</button>
+                  </div>
+                  <div className="action-card">
+                    <h3>ARE YOU A LUCKY WINNER?</h3>
+                    <button>WITHDRAW YOUR PRIZE!</button>
+                  </div>
+                </div>
+                {/* <div className="place-bet interaction-card">
+                  <h3>PLACE YOUR BET</h3>
+                  <input 
+                    type="number" 
+                    placeholder="1 - 50" 
+                    value={betNumber}
+                    onChange={(e) => setBetNumber(Number(e.target.value))}
+                  />
+                  <Bet betNumber={Number(betNumber)} />
+                </div>
+                <div className="withdraw-prize interaction-card">
+                  <h3>ARE YOU A LUCKY WINNER?</h3>
+                  <button className="withdraw-prize">Withdraw Prize!</button>
+                </div> */}
+              </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
